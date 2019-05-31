81928a3a37cd6bc3813c323db25ecdadab490e61
undersfx <atchin_@hotmail.com>
2019-05-31 14:21:54 -0300

    adiciona métodos estáticos e de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,34 @@
 class Pessoa:
-    #Atributo de Classe
+    # Atributo de Classe
     olhos = 2
 
+    # Método Inicializador
     def __init__(self, *filhos, nome=None, idade=None):
+        # Atributos de Instância
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
 
+    # Método
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    # Método Estático
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    
+    # Método de Classe
+    @classmethod
+    def metodo_de_classe(cls):
+        return f'Classe: {cls}, olhos: {cls.olhos}'
 
 if __name__ == "__main__":
     fulano = Pessoa(nome='Fulano')
     ciclano = Pessoa(nome='Ciclano')
     thiago = Pessoa(fulano, ciclano, nome='Thiago', idade=27)
 
-    # Chamada de Métodos
+    # Métodos
     print(Pessoa.cumprimentar(thiago))
     print(thiago.cumprimentar())
     print(id(thiago))
@@ -44,3 +57,11 @@ if __name__ == "__main__":
     print(thiago.__dict__)
     del thiago.olhos
     print(thiago.olhos)
+
+    # Métodos Estáticos
+    print(Pessoa.metodo_estatico())
+    print(thiago.metodo_estatico())
+
+    # Métodos de Classe 
+    print(Pessoa.metodo_de_classe())
+    print(thiago.metodo_de_classe())
