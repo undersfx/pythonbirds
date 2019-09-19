49aa25f2da0c3d1ca77d975289b08748045225f3
undersfx <atchin_@hotmail.com>
2019-09-19 00:11:34 -0300

    Adiciona documentação e doctest da classe Carro

---
+++ oo/carro.py
@@ -0,0 +1,97 @@
+"""
+Criar uma casse carro que possua dois atributos compostos por outras duas classes:
+
+1 Motor
+2 Direção
+
+Motor: Controla a velocidade do carro.
+Atributos de dado: Velocidade
+Métodos: Acelerar (+1 velocidade), Frear (-2 velocidade)
+
+Direção: Controlar a direção do carro.
+Atributos de dado: Direção 
+
+4 pontos:
+  N
+O   L
+  S
+
+Métodos: Virar a Direita, Virar a Esquerda
+
+Exemplos:
+
+# Doctest Motor
+>>> motor = Motor()
+>>> motor.velocidade
+0
+>>> motor.acelerar()
+>>> motor.velocidade
+1
+>>> motor.acelerar()
+>>> motor.velocidade
+2
+>>> motor.acelerar()
+>>> motor.velocidade
+3
+>>> motor.frear()
+>>> motor.velocidade
+1
+>>> motor.frear()
+>>> motor.velocidade
+0
+
+# Doctest Direção
+>>> direcao = Direcao()
+>>> direcao.valor
+"Norte"
+>>> direcao.virar_a_direita()
+>>> direcao.valor
+"Leste"
+>>> direcao.virar_a_direita()
+>>> direcao.valor
+"Sul"
+>>> direcao.virar_a_direita()
+>>> direcao.valor
+"Oeste"
+>>> direcao.virar_a_direita()
+>>> direcao.valor
+"Norte"
+>>> direcao.virar_a_esquerda()
+>>> direcao.valor
+"Oeste"
+>>> direcao.virar_a_esquerda()
+>>> direcao.valor
+"Sul"
+>>> direcao.virar_a_esquerda()
+>>> direcao.valor
+"Leste"
+>>> direcao.virar_a_esquerda()
+>>> direcao.valor
+"Norte"
+
+# Dcotest Carro
+>>> carro = Carro(direcao, motor)
+>>> carro.calcular_velocidade()
+0
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
+1
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
+2
+>>> carro.frear()
+>>> carro.calcular_velocidade()
+0
+>>> carro.calcular_direção()
+"Norte"
+>>> carro.virar_a_direita()
+>>> carro.calcular_direção()
+"Leste"
+>>> carro.virar_a_esquerda()
+>>> carro.calcular_direção()
+"Norte"
+>>> carro.virar_a_esquerda()
+>>> carro.calcular_direção()
+"Oeste"
+
+"""
