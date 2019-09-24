07f147d44d388987f8cf1a2e328628b619024ba5
undersfx <atchin_@hotmail.com>
2019-09-24 03:03:01 -0300

    Implementa calculo de posicao y em Passaro

--- atores.py
+++ atores.py
@@ -121,7 +121,18 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1, 1
+        if self.foi_lancado():
+            delta_t = tempo - self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+
+        return super().calcular_posicao(tempo)
+
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual = self._y_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
+        self.y = y_atual
 
     def lancar(self, angulo, tempo_de_lancamento):
         """
@@ -137,7 +148,9 @@ class Passaro(Ator):
 
 
 class PassaroAmarelo(Passaro):
-    pass
+    _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
